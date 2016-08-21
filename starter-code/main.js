616bb9e7c434b0a92180cf4f447ed6ce82a3a74d
nishkixs <nishon.radhakrishnan@gmail.com>
2016-08-21 19:10:47 -0400

    Final Matching Game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,29 +1,62 @@
-console.log("JS file is connected to HTML! Woo!")
+/*
+JavaScript Event Listeners with console logging
+*/
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// King Imgur URL = http://imgur.com/a/ho31z
+// Queem Imgur URL = http://imgur.com/a/Q5wm1
 
+var kingPicture = '<img src="king.jpg" width="150" height="200">';
+var queenPicture = '<img src="queen.jpg" width="150" height="200">';
+var cardsFlipped = [];
 
-if (cardFour === cardTwo) {
-    alert ("Not a match");
+var card1Listener = document.getElementById("card1");
+card1Listener.addEventListener("click", clickCard);
+
+function clickCard() {
+console.log("The first card, a KING, was selected.");
+document.getElementById('card1').innerHTML = kingPicture;
+setTimeout(function(){ document.getElementById('card1').innerHTML = ''}, 3000);
+console.log(cardsFlipped);
+cardsFlipped.push(parseInt('1'));
+console.log("Number of cards flipped: " + cardsFlipped.length);
+
 }
 
-if (cardOne === cardTwo) {
-    alert ("You found a match!");
+var card2Listener = document.getElementById("card2");
+card2Listener.addEventListener("click", clickCard2);
+
+function clickCard2() {
+console.log("The second card, a QUEEN was selected");
+document.getElementById('card2').innerHTML = queenPicture;
+setTimeout(function(){ document.getElementById('card2').innerHTML = ''}, 3000);
+cardsFlipped.push(parseInt('2'));
+console.log(cardsFlipped);
+console.log("Number of cards flipped: " + cardsFlipped);
+
 }
 
-var gameBoard = document.getElementById("game-board");
+var card3Listener = document.getElementById("card3");
+card3Listener.addEventListener("click", clickCard3);
 
-var createBoard= function(){
-    for (var i=0; i<4; i++){
- var newCard = document.createElement('div');
-     newCard.className = "board";
-     game-board.appendChild(newCard);
-    }
-    
-createBoard();
-    
+function clickCard3() {
+console.log("The third card, a KING, was selected");
+document.getElementById('card3').innerHTML = kingPicture;
+setTimeout(function(){ document.getElementById('card3').innerHTML = ''}, 3000);
+console.log(cardsFlipped);
+cardsFlipped.push(parseInt('3'));
 }
 
+var card4Listener = document.getElementById("card4");
+card4Listener.addEventListener("click", clickCard4);
+
+function clickCard4() {
+console.log("The fourth card, a QUEEN, was selected");
+document.getElementById('card4').innerHTML = queenPicture;
+setTimeout(function(){ document.getElementById('card4').innerHTML = ''}, 3000);
+cardsFlipped.push(parseInt('4'));
+console.log(cardsFlipped);
+}
+
+if (cardsFlipped.length == 1) {
+console.log("Number of cards flipped: ")
+}
